55c7a48cb9e82173632e7cbe3cb140202511360b
Alexey Bondar <a.bondar@student.csn.khai.edu>
2020-04-19 14:12:37 +0300

    Chaged config file

--- web-client/src/config.js
+++ web-client/src/config.js
@@ -1,4 +1,4 @@
 export default {
-    //apiDomain: "http://10.40.236.148:5000"
-    apiDomain: "http://localhost:61551"
+    apiDomain: "http://hotelbooking.us-east-1.elasticbeanstalk.com"
+    //apiDomain: "http://localhost:61551"
 }
